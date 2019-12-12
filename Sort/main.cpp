2950e8b8d9c8d462f4c28a3fdd34ccd52aa0e23d
Karol-Perec <karol.perec@gmail.com>
2019-12-12 14:24:58 +0100

    sort

--- Sort/main.cpp
+++ Sort/main.cpp
@@ -1,90 +1,87 @@
-#include <algorithm>
 #include <vector>
 #include <omp.h>
 #include <random>
 #include <iostream>
-#include <functional>
 
 using namespace std;
 
-std::vector<int> merge(const std::vector<int> &left, const std::vector<int> &right, std::vector<int> &original) {
-    int l = 0;
-    int r = 0;
+std::vector<int> merge(const std::vector<int> &leftArray, const std::vector<int> &rightArray, std::vector<int> &array) {
+    auto l = 0;
+    auto r = 0;
 
-    //wykonujemy petle while dopoki vektory do złaczenia nie beda puste
-    //porownujemy i wpisujemy w odpowiednie miejsce
-    while (l < left.size() && r < right.size()) {
-        if (left.at(l) <= right.at(r)) {
-            original.at(l + r) = left.at(l);
+    while (l < leftArray.size() && r < rightArray.size()) {
+        if (leftArray[l] <= rightArray[r]) {
+            array[l + r] = leftArray[l];
             l++;
         } else {
-            original.at(l + r) = right.at(r);
+            array[l + r] = rightArray[r];
             r++;
         }
     }
 
-    //jezeli pozostaly niedopasowane to jest wpisuje na koniec
-    for (; l < left.size(); l++) {
-        original.at(l + r) = left.at(l);
+    while (l < leftArray.size()) {
+        array[l + r] = leftArray.at(l);
+        l++;
     }
 
-    for (; r < right.size(); r++) {
-        original.at(l + r) = right.at(r);
+    while (r < rightArray.size()) {
+        array[l + r] = rightArray.at(r);
+        r++;
     }
 
-    return original;
+    return array;
 }
 
 std::vector<int> mergeSort(std::vector<int> &array) {
-    if (array.size() < 2) {
+    if (array.size() == 1) {
         return array;
     } else {
-        auto middle = int(array.size() / 2);
-        std::vector<int> left(array.begin(), array.begin() + middle);
-        std::vector<int> right(array.begin() + middle, array.end());
-        return merge(mergeSort(left), mergeSort(right), array);
+        auto splitPoint = int(array.size() / 2);
+        std::vector<int> leftArray(array.begin(), array.begin() + splitPoint);
+        std::vector<int> rightArray(array.begin() + splitPoint, array.end());
+
+        return merge(mergeSort(leftArray), mergeSort(rightArray), array);
     }
 }
 
-std::vector<int> mergeParallel(const std::vector<std::vector<int>> &vectors) {
-    //sprawdza ile wektorów, jak jeden to nie ma co mergowac
-    if (vectors.size() < 2) {
-        return vectors.at(0);
+std::vector<int> mergeThreadArrays(const std::vector<std::vector<int>> &arraysToMerge) {
+    if (arraysToMerge.size() == 1) {
+        return arraysToMerge[0];
     } else {
-        std::vector<std::vector<int>> reduced(vectors.size() / 2);
+        std::vector<std::vector<int>> mergedArrays(arraysToMerge.size() / 2);
 
-#pragma omp parallel shared(vectors, reduced) default(none)
+        omp_set_num_threads(int(arraysToMerge.size() / 2));
+#pragma omp parallel shared(arraysToMerge, mergedArrays) default(none)
         {
-            int current_thread = omp_get_thread_num();
-            try {
-                std::vector<int> a = vectors.at(current_thread * 2);
-                std::vector<int> b = vectors.at(current_thread * 2 + 1);
-                std::vector<int> placeholder(a.size() + b.size());
-                reduced.at(current_thread) = merge(a, b, placeholder);
-            } catch (const std::out_of_range &e) {}
-        }
+            const auto threadId = omp_get_thread_num();
 
-        return mergeParallel(reduced);
+            auto leftArray = arraysToMerge[2 * threadId];
+            auto rightArray = arraysToMerge[2 * threadId + 1];
+            std::vector<int> mergedArray(leftArray.size() + rightArray.size());
+
+            mergedArrays[threadId] = merge(leftArray, rightArray, mergedArray);
+        }
+        return mergeThreadArrays(mergedArrays);
     }
 }
 
-std::vector<int> mergeSortParallel(std::vector<int> &array) {
-    const auto element_for_thread = int(array.size() / omp_get_max_threads());
-    std::vector<std::vector<int>> sub_problem(omp_get_max_threads());
+std::vector<int> mergeSortParallel(const std::vector<int> &array) {
+    const auto maxThreads = omp_get_max_threads();
+    const auto numbersPerThread = int(array.size() / maxThreads);
+    std::vector<std::vector<int>> threadArrays(maxThreads);
 
-#pragma omp parallel shared(array, sub_problem) default(none)
+#pragma omp parallel shared(array, threadArrays) default(none)
     {
-        int current_thread = omp_get_thread_num();
-        auto start = array.begin() + (current_thread * element_for_thread);
-        auto stop = array.begin() + ((current_thread + 1) * element_for_thread);
+        const auto threadId = omp_get_thread_num();
+        auto begin = array.begin() + (threadId * numbersPerThread);
+        auto end = array.begin() + ((threadId + 1) * numbersPerThread);
+        std::vector<int> threadArray(begin, end);
 
-        std::vector<int> sub_array(start, stop);
-        mergeSort(sub_array);
+        mergeSort(threadArray);
 
-        sub_problem.at(current_thread) = sub_array;
+        threadArrays[threadId] = threadArray;
     }
-
-    return mergeParallel(sub_problem);
+    return mergeThreadArrays(threadArrays);
 }
 
 std::vector<int> generateUnsortedArray(int size) {
@@ -100,13 +97,13 @@ std::vector<int> generateUnsortedArray(int size) {
 }
 
 int main() {
-    const auto size = 10000;
+    const auto size = 100000;
     auto unsortedArray = generateUnsortedArray(size);
     auto unsortedArray2{unsortedArray};
 
     auto start_time = omp_get_wtime();
     unsortedArray = mergeSortParallel(unsortedArray);
-    cout << "Parallel merge sort TIME: " << omp_get_wtime() - start_time << endl;
+    cout << "  Parallel merge sort TIME: " << omp_get_wtime() - start_time << endl;
 
     start_time = omp_get_wtime();
     mergeSort(unsortedArray2);
